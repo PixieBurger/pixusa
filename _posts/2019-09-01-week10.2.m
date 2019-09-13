86d042197ffc92c68e47e048031757b71ce06547
Szili Timea <42409828+PixieBurger@users.noreply.github.com>
2019-09-13 21:18:43 +0200

    Create 2019-09-01-week10.2.m

---
+++ _posts/2019-09-01-week10.2.m
@@ -0,0 +1,15 @@
+---
+
+layout: post
+
+title:  'Week #10 When autumn comes'
+
+date:   2019-09-01
+
+img:  9_m.jpg # Add image post (optional)
+
+tags: [Blog, Washington,  Hiking, Homeless, Tent]
+
+author: Timea Szili # Add name author (optional)
+
+---
